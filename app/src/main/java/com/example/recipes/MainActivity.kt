65c5a802d77e86cb184925dfea741b99b2c271f1
Maciej Wieczorek <maciejmail.wieczorek@gmail.com>
2023-05-12 22:12:18 +0200

    Tablet version

--- app/src/main/java/com/example/recipes/MainActivity.kt
+++ app/src/main/java/com/example/recipes/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity(), RecipeListFragment.Listener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +13,20 @@ class MainActivity : AppCompatActivity(), RecipeListFragment.Listener {
     }
 
     override fun itemClicked(id: Long) {
-        val intent = Intent(this, DetailActivity::class.java);
-        intent.putExtra(DetailActivity.EXTRA_RECIPE_ID, id.toInt())
-        startActivity(intent);
+        val fragmentContainer: View? = findViewById(R.id.fragment_container)
+        if (fragmentContainer != null) {
+            val details = RecipeDetailFragment()
+            val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+            details.setRecipe(id)
+            ft.replace(R.id.fragment_container, details)
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            ft.addToBackStack(null)
+            ft.commit()
+        }
+        else {
+            val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra(DetailActivity.EXTRA_RECIPE_ID, id.toInt())
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/recipes/RecipeDetailFragment.kt
+++ app/src/main/java/com/example/recipes/RecipeDetailFragment.kt
@@ -10,24 +10,35 @@ import android.widget.TextView
 class RecipeDetailFragment : Fragment() {
 
     private var recipeId: Long = 0
-    private lateinit var rootView: View;
+    private lateinit var rootView: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState != null) {
+            recipeId = savedInstanceState.getLong("recipeId")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         rootView = inflater.inflate(R.layout.fragment_recipe_detail, container, false)
-        return rootView;
+        return rootView
     }
 
     override fun onStart() {
         super.onStart()
-        val recipe: Recipe = Recipe.recipes[recipeId.toInt()];
+        val recipe: Recipe = Recipe.recipes[recipeId.toInt()]
         val title: TextView = rootView.findViewById<TextView>(R.id.textTitle)
         title.text = recipe.getName()
         val description: TextView = rootView.findViewById<TextView>(R.id.textDescription)
         description.text = recipe.getRecipe()
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putLong("recipeId", recipeId)
     }
 
     fun setRecipe(id: Long) {
