e609f5d6af806d28d82d12140a212f1040d9b767
Maciej Wieczorek <maciejmail.wieczorek@gmail.com>
2023-06-11 14:34:23 +0200

    Logo

--- app/src/main/java/com/example/recipes/TopFragment.kt
+++ app/src/main/java/com/example/recipes/TopFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.graphics.drawable.toDrawable
 
 class TopFragment : Fragment() {
 
@@ -17,6 +19,10 @@ class TopFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_top, container, false)
+        val view = inflater.inflate(R.layout.fragment_top, container, false)
+        val logoView = view.findViewById<ImageView>(R.id.logo)
+        val drawable = resources.getDrawable(R.drawable.logo)
+        logoView.setImageDrawable(drawable)
+        return view
     }
 }
