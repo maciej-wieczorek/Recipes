9229e7d10dff6b61ce8c17b9426393cf2bea7c53
Maciej Wieczorek <maciejmail.wieczorek@gmail.com>
2023-06-11 21:52:15 +0200

    Splash screen with animation

---
+++ app/src/main/java/com/example/recipes/SplashActivity.kt
@@ -0,0 +1,98 @@
+package com.example.recipes
+
+import android.Manifest
+import android.animation.ObjectAnimator
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.animation.LinearInterpolator
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class SplashActivity : AppCompatActivity() {
+
+    private val INTERNET_PERMISSION_REQUEST_CODE = 123 // Use any unique request code
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setColorFilter(getColor(R.color.purple_200))
+        val rotationAnimator = ObjectAnimator.ofFloat(imageView, "rotation", 0f, 360f)
+        rotationAnimator.duration = 2000
+        rotationAnimator.repeatCount = ObjectAnimator.INFINITE
+        rotationAnimator.interpolator = LinearInterpolator()
+        rotationAnimator.start()
+
+        val loadingThread = object : Thread() {
+            override fun run() {
+                try {
+                    if (isFirstLaunch()) {
+                        fetchData()
+                    }
+                    sleep(500)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+        loadingThread.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val sharedPrefs = getSharedPreferences("Prefs", Context.MODE_PRIVATE)
+        sharedPrefs.edit().putBoolean("FirstLaunch", true).apply()
+    }
+
+    private fun isFirstLaunch(): Boolean {
+        val sharedPrefs = getSharedPreferences("Prefs", Context.MODE_PRIVATE)
+        val isFirstLaunch = sharedPrefs.getBoolean("FirstLaunch", true)
+        if (isFirstLaunch) {
+            sharedPrefs.edit().putBoolean("FirstLaunch", false).apply()
+        }
+        return isFirstLaunch
+    }
+
+    private fun fetchData() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.INTERNET
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission not granted, request it
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.INTERNET),
+                INTERNET_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            // Permission already granted
+            Recipe.getRecipes()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == INTERNET_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Recipe.getRecipes()
+            } else {
+                // Permission denied
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/recipes/TopFragment.kt
+++ app/src/main/java/com/example/recipes/TopFragment.kt
@@ -6,13 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.graphics.drawable.toDrawable
-
+import androidx.core.content.ContextCompat.getColor
 class TopFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -21,6 +19,7 @@ class TopFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_top, container, false)
         val logoView = view.findViewById<ImageView>(R.id.logo)
+        logoView.setColorFilter(getColor(view.context, R.color.purple_200))
         val drawable = resources.getDrawable(R.drawable.logo)
         logoView.setImageDrawable(drawable)
         return view
